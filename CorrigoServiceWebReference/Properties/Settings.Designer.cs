73e6f10e896b84b95c492e6c7df2495d0d89b40f
Oleksii Anisenko <oleksii.anisenko@corrigo.com>
2021-08-04 19:36:56 +0300

    Update WebReference to new web service

--- CorrigoServiceWebReference/Properties/Settings.Designer.cs
+++ CorrigoServiceWebReference/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace CorrigoServiceWebReference.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,7 +26,7 @@ namespace CorrigoServiceWebReference.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.WebServiceUrl)]
-        [global::System.Configuration.DefaultSettingValueAttribute("http://wt90gap.corrigo.com/wsdk/CorrigoService.asmx")]
+        [global::System.Configuration.DefaultSettingValueAttribute("http://am-ce97a.corrigo.com/wsdk/CorrigoService.asmx")]
         public string CorrigoServiceWebReference_CorrigoGA_CorrigoService {
             get {
                 return ((string)(this["CorrigoServiceWebReference_CorrigoGA_CorrigoService"]));
