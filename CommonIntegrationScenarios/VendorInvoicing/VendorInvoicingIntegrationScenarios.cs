40825f19accf33201bd16c0a046e9cbf535e8700
Oleksii Anisenko <oleksii.anisenko@corrigo.com>
2021-08-05 18:18:25 +0300

    Updates for ApInvoiceStatus in UpdateInvoiceStatus

--- CommonIntegrationScenarios/VendorInvoicing/VendorInvoicingIntegrationScenarios.cs
+++ CommonIntegrationScenarios/VendorInvoicing/VendorInvoicingIntegrationScenarios.cs
@@ -18,7 +18,33 @@ namespace CommonIntegrationScenarios.VendorInvoicing
             // Update the status of specified invoices to "Exported"
             // 
             int[] invoiceIDs = approvedInvoices.Select(i => i.Id).Take(2).ToArray();
-            InvoiceScenario.UpdateInvoiceStatus(corrigoService, invoiceIDs, ApState.Exported);
+
+            PropertySet _propertySet = new PropertySet
+            {
+                Properties = new string[]
+                            {
+                                "Id",
+                                "DisplayAs",
+                                "Description",
+                                "ForecastGroupId",
+                                "ApStateId",//
+                                "ReadyForExport",
+                                "SetOnExport",
+                                "ChildWoDefault"
+                            }
+            };
+            var query = new QueryByProperty
+            {
+                EntityType = EntityType.ApInvoiceStatus,
+                PropertySet = _propertySet,//new AllProperties(),
+                Conditions = new[]
+                    {
+                        new PropertyValuePair {PropertyName = "ApInvoiceStatus", Value = ApState.Exported},
+                    },
+            };
+            var invoiceStatuses = corrigoService.RetrieveMultiple(query).Cast<ApInvoiceStatus>().ToArray();
+
+            InvoiceScenario.UpdateInvoiceStatus(corrigoService, invoiceIDs, invoiceStatuses[0].ApStateId);
 
             //
             // Vendor Invoice Payment
