fdb52d548189f30285cb4f7f59307b31fea14057
Oleksii Anisenko <oleksii.anisenko@corrigo.com>
2021-08-05 18:43:21 +0300

    Integration test credentials update

--- CommonIntegrationScenarios/Program.cs
+++ CommonIntegrationScenarios/Program.cs
@@ -5,10 +5,14 @@ namespace CommonIntegrationScenarios
 {
     internal class Program
     {
-        private const string _url = "http://v91.qa.corrigo.com/wsdk/CorrigoService.asmx";
-        private const string _company = "Integrations 9.1 DB";
-        private const string _userName = "ewi";
-        private const string _password = "Corrigo2!";
+        //private const string _url = "http://v91.qa.corrigo.com/wsdk/CorrigoService.asmx";
+        //private const string _company = "Integrations 9.1 DB";
+        //private const string _userName = "ewi";
+        //private const string _password = "Corrigo2!";
+        private const string _url = "http://qa-am-ent-f2.corrigo-qa.com/wsdk/CorrigoService.asmx";
+        private const string _company = "Integrations Tests";
+        private const string _userName = "wsdk";
+        private const string _password = "1234";
 
         private static readonly CorrigoClientProvider _clientProvider = new CorrigoClientProvider();
 
